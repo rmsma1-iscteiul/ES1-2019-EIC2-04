be2ce796690525561eb01096737723db1bcb174b
Menoita99 <rmsma1@iscte-iul.pt>
2019-11-06 10:49:24 +0000

    Initial Commit GUI

--- EngenhariaDeSoftware/src/main/java/project/gui/controllers/Controller.java
+++ EngenhariaDeSoftware/src/main/java/project/gui/controllers/Controller.java
@@ -1,11 +1,19 @@
 package project.gui.controllers;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -14,9 +22,10 @@ import javafx.stage.Stage;
  * 
  * @author RuiMenoita
  */
-public class Controller extends Application{
+public class Controller extends Application implements Initializable{
 
-	
+	private Stage window;
+	private List<String> historico  = new ArrayList<String>();
 	
 	
 	/**
@@ -25,7 +34,18 @@ public class Controller extends Application{
 	 */
     @FXML
     public void openFile(ActionEvent event) {
-    	System.out.println("YEAH ! IM A BADASS PROGRAMMER!");
+    	FileChooser fc = new FileChooser();
+    	fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("excel", "*.xlsx"));
+    	File selectedFile = fc.showOpenDialog(window);
+    	System.out.println(selectedFile.getAbsolutePath());
+    	historico.add(selectedFile.getAbsolutePath());
+    }
+    
+    public void openRecent(ActionEvent event) {
+    	//FileChooser fc = new FileChooser();
+    	for(String n : historico) {
+    		System.out.println(n);
+    	}
     }
 	
 	
@@ -40,15 +60,21 @@ public class Controller extends Application{
 	 */
 	@Override
 	public void start(Stage window) throws Exception {
+		this.window = window;
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("scene.fxml")); 
 		Parent node = loader.load();
 		Scene scene = new Scene(node);
+		
 
 		window.setScene(scene);
 		window.show();
 	}
 	
-	
+
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	public static void show(String[] args) {
