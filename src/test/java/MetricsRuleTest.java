057119ff8a5f148f305c058dabfe32a1971632eb
mfvmo-iscteiul <mfvmo@iscte.iul.pt>
2019-12-13 21:30:09 +0000

    metrics junit test

---
+++ src/test/java/MetricsRuleTest.java
@@ -0,0 +1,158 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MetricsRuleTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testMetricsRuleConstructor1() {
+		
+	}
+
+	@Test
+	void testMetricsRuleConstuructor2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLocValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLocValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLocComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLocComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLocCycloAndOr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLocCycloAndOr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCycloValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCycloValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCycloComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCycloComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAftdValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAftdValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAftdComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAftdComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAftdLaaAndOr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAftdLaaAndOr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLaaValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLaaValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLaaComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLaaComparison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMetricName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMetricName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFileToString() {
+		fail("Not yet implemented");
+	}
+
+}
